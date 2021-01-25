d7dba0a9437d2d76d963ef5af2a40ce98147340a
Pablo Peuscovich <pp@qbkconsulting.com>
2021-01-25 20:02:10 -0300

    TICKET-03: Add BakerJob

--- app/jobs/baker_job.rb
+++ app/jobs/baker_job.rb
@@ -1,7 +1,9 @@
 class BakerJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(cookie_id)
+    sleep(120)
+    cookie = Cookie.find_by(id: cookie_id)
+    cookie&.update(status: 'cooked')
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,8 @@ module FlowspaceBakery
         request_specs: false
       g.fixture_replacement :factory_girl, dir: "spec/factories"
     end
-
+    
+    config.active_job.queue_adapter = :sidekiq
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
